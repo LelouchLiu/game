72870beb40ef9a0c2a31f0a7cd67a2a9d4d47fee
zcr1 <zcr1@pitt.edu>
2013-03-27 22:56:41 -0400

    Server start-up complete

--- server.py
+++ server.py
@@ -1,18 +1,24 @@
 #Entry point to start the server
+
 import sys
 from twisted.internet import reactor
 from twisted.application.service import MultiService
 from twisted.application.service import IServiceMaker
 from twisted.application.internet import TCPServer
-#from server.server_network import GameFactory
-#from server.server_world import *
+from server.server_world import *
+from server.server_network import GameFactory
 
 #sys.path.append('data')
+port = 1338
+host = 'localhost'
 
 def main():
-	print 'Server Started'
-	#world = World(granularity=100, platformClock=reactor)
-	#factory = GameFactory(world)
+	print 'Server Started on port ',port
+
+	world = World(granularity=100, platformClock=reactor)
+	service = GameService(world)
+	factory = GameFactory(world)
+
 	reactor.listenTCP(1338, factory)	
 	reactor.run()
 
