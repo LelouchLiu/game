21abb2fd742fd43c9742efb9cbd6a7aa2e6ca4de
Zach <zachreinhardtguy@gmail.com>
2013-04-11 21:34:37 -0400

    Started entity refactoR

--- client/projectile.py
+++ client/projectile.py
@@ -5,7 +5,7 @@ import pymunk
 from math import sin,cos,sqrt
 from pymunk import Vec2d
 
-class Projectile(pygame.sprite.Sprite):
+class Projectile(pygame.sprite.Sprite, Entity):
 	
 	#temporary values
 	velocity = 70
@@ -31,26 +31,10 @@ class Projectile(pygame.sprite.Sprite):
 		self.friendlyFire = friendlyFire
 		self.seconds = seconds
 		self.vector = Vec2d(cos(orientation), sin(orientation))
-		self.rotate(orientation)
-		self._setPhysics()
+		#self.rotate(orientation)
+		#self._setPhysics()
+		self.applyImpulse()
 
-	def _setPhysics(self):
-		#pymunk initializations
-		self.inertia = pymunk.moment_for_circle(self.mass, 0, self.radius) #mass, width, height
-		self.body = pymunk.Body(self.mass,  self.inertia) #Mass, Moment of inertia
-		self.shape = pymunk.Circle(self.body, self.radius)
-		self.body.position = pymunk.Vec2d(self.bodyPos[0], self.bodyPos[1])
-		self.body._set_velocity_limit(self.maxVel)
-		self.shape.elasticity = self.elasticity
-		self.applyForce()
-
-	#Apply the intial thrust force to object
-	def applyForce(self):	
-		thrust = self.velocity
-		#multiplied by 100 because it seems to be slow
-		force = pymunk.Vec2d(100 * thrust * self.vector.x, 100 *thrust * self.vector.y)
-		offset = [0, 0]
-		self.body.apply_impulse(force, r=offset)
 
 	#Returns true if projectile has traveled its maximum range thus far	
 	def distanceTraveled(self):
@@ -63,7 +47,7 @@ class Projectile(pygame.sprite.Sprite):
 
 	def rotate(self, orientation):
 		#Why do I use 57 again? Figure that one out lol
-		oldPos = copy.deepcopy(self.rect.center)
+		#oldPos = copy.deepcopy(self.rect.center)
 		self.image = pygame.transform.rotate(self.origImage, self.toDegrees(self.orientation))
-		self.rect = self.image.get_rect(center=oldPos)
+		#self.rect = self.image.get_rect(center=oldPos)
 		self.body._set_angle(self.orientation)
